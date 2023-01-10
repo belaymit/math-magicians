27e4ae5e002bdc8b34f1a6fcb8c081fa39cfee84
belaymit <2belamit@gmail.com>
2023-01-10 09:33:06 +0300

    imported all pages and components

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Navbar from './components/navbar/Navbar';
+import Home from './pages/home/Home';
+import Calculations from './pages/calculations/Calculations';
+import Quotes from './pages/quotes/Quotes';
+import Footer from './components/footer/Footer';
+
+const App = () => (
+  <>
+    <BrowserRouter>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/calculator" element={<Calculations />} />
+        <Route path="/quotes" element={<Quotes />} />
+      </Routes>
+      <Footer />
+    </BrowserRouter>
+  </>
+);
+
+export default App;
