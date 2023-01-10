2c3adf363cffc6311e8db6a219950fde87c21b22
belaymit <2belamit@gmail.com>
2023-01-10 09:26:42 +0300

    added dynamic menu-list items

---
+++ src/components/navbar/menuList.js
@@ -0,0 +1,7 @@
+const menuList = [
+  { id: 1, link: '/', title: 'Home' },
+  { id: 2, link: '/calculator', title: 'Calculator' },
+  { id: 3, link: '/quotes', title: 'Quotes' },
+];
+
+export default menuList;
