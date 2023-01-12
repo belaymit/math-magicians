4e329aec9ab8575fefcca902a6fca43822d3c4f7
belaymit <2belamit@gmail.com>
2023-01-12 20:30:02 +0300

    added Quote to quotes-page

--- src/pages/quotes/Quotes.jsx
+++ src/pages/quotes/Quotes.jsx
@@ -2,7 +2,16 @@ import React from 'react';
 import './quotes.scss';
 
 const Quotes = () => (
-  <section className="quotes-container">Quotes</section>
+  <section className="quotes-container">
+    <div className="quotes">
+      <p>
+        Mathematics is not about numbers, equations, computations,
+        or algorithms: it is about understanding.
+        {' '}
+        <span>William Paul Thurston</span>
+      </p>
+    </div>
+  </section>
 );
 
 export default Quotes;
