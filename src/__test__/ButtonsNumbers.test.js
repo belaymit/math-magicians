0f25472cdc2865c0a603c83250890ceabbf1d3ae
belaymit <2belamit@gmail.com>
2023-01-19 18:20:00 +0300

    fixed linter error

--- src/__test__/ButtonsNumbers.test.js
+++ src/__test__/ButtonsNumbers.test.js
@@ -1,36 +1,38 @@
-import ButtonsNumbers from "../components/buttonsNumbers/ButtonsNumbers";
 import { render, screen, fireEvent } from '@testing-library/react';
-require('@testing-library/jest-dom');
-describe('Testing the Button Number Component',()=>{
-    const handleClick = jest.fn();
+import ButtonsNumbers from '../components/buttonsNumbers/ButtonsNumbers';
 
-    it('Checking for Number to be Render without any error',()=>{
-        const btn = render(
-            <ButtonsNumbers handleClick = {handleClick} />
-        );
-        expect(btn).toMatchSnapshot();
-    });
-    
-    it('Checking for +/- Buttons to be Render on the screen',()=>{
-        render(
-            <ButtonsNumbers handleClick = {handleClick} />
-        );
-        expect(screen.getByText('+/-')).toBeInTheDocument();
-    });
-    
-    it('Checking for User Interaction',()=>{
-        render(
-            <ButtonsNumbers handleClick = {handleClick} />
-        );
-        const output = screen.getByText('5')
-        fireEvent.click(output);
-    });
-    it('Checking for Button Interaction',()=>{
-        render(
-            <ButtonsNumbers handleClick = {handleClick} />
-        );
-        const output = screen.getByText('5')
-        fireEvent.click(output);
-        expect(handleClick).toHaveBeenCalled();
-    });
+require('@testing-library/jest-dom');
+
+describe('Testing the Button Number Component', () => {
+  const handleClick = jest.fn();
+
+  it('Checking for Number to be Render without any error', () => {
+    const btn = render(
+      <ButtonsNumbers handleClick={handleClick} />,
+    );
+    expect(btn).toMatchSnapshot();
+  });
+
+  it('Checking for +/- Buttons to be Render on the screen', () => {
+    render(
+      <ButtonsNumbers handleClick={handleClick} />,
+    );
+    expect(screen.getByText('+/-')).toBeInTheDocument();
+  });
+
+  it('Checking for User Interaction', () => {
+    render(
+      <ButtonsNumbers handleClick={handleClick} />,
+    );
+    const output = screen.getByText('5');
+    fireEvent.click(output);
+  });
+  it('Checking for Button Interaction', () => {
+    render(
+      <ButtonsNumbers handleClick={handleClick} />,
+    );
+    const output = screen.getByText('5');
+    fireEvent.click(output);
+    expect(handleClick).toHaveBeenCalled();
+  });
 });
--- src/__test__/DisplayItems.test.js
+++ src/__test__/DisplayItems.test.js
@@ -4,14 +4,14 @@ import DisplayItems from '../components/displayItems/DisplayItems';
 require('@testing-library/jest-dom');
 
 describe('Test for DisplayItems component', () => {
-    const output = '5';
+  const output = '5';
   it('renders Output without any error', () => {
-    const result = render(<DisplayItems output={output}/>);
+    const result = render(<DisplayItems output={output} />);
     expect(result).toMatchSnapshot();
   });
 
-  it("renders Numeric Output without any error ", () => {
-    render(<DisplayItems output={output}/>);
+  it('renders Numeric Output without any error ', () => {
+    render(<DisplayItems output={output} />);
     expect(screen.getByText('5')).toBeInTheDocument();
   });
 });
