45deb1ee445ec29b41a960605116ffecbc2d9fbf
ahadb802 <abdulahadb802@gmail.com>
2023-01-19 19:57:28 +0500

    Update footer test case

--- src/__test__/Footer.test.js
+++ src/__test__/Footer.test.js
@@ -5,12 +5,12 @@ import Footer from '../components/footer/Footer';
 require('@testing-library/jest-dom');
 
 describe('Test for Footer component', () => {
-  it('renders Quote component without any error', () => {
+  it('renders Footer component without any error', () => {
     const output = render(<BrowserRouter><Footer /></BrowserRouter>);
     expect(output).toMatchSnapshot();
   });
 
-  it("renders Quote component with the correct text 'Lorem ipsum dolor sit amet consectetur adipisicing elit.' ", () => {
+  it("renders Footer component with the correct text 'Lorem ipsum dolor sit amet consectetur adipisicing elit.' ", () => {
     render(<BrowserRouter><Footer /></BrowserRouter>);
     expect(screen.getByText('Lorem ipsum dolor sit amet consectetur adipisicing elit.')).toBeInTheDocument();
   });
